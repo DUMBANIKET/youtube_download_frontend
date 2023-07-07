0ce980a7ef303315b3dbca06df8b1f9ba038c015
TRUTHANIKET <aniketsingh78698@gmail.com>
2023-07-07 21:08:13 +0530

    adding stuffs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import './style.css'
+
 
 function App() {
+   const [loading,setLoading]=useState('')
+   const [count,setCount]=useState(0)
+  const [post,setPosts]=useState([])
+  const [vidurl,setVidurl]=useState('')
+  const [status,setStatus]=useState('nothing here search to get the url')
+const [err,setErr]=useState('')
+  const url="https://youtu.be/-wOkkbufYIA"
+  const api_url="https://youtube-api-0own.onrender.com/video"
+const subm=async(e)=>{
+   setStatus('')
+  e.preventDefault();
+  setLoading("Loading please wait")
+      fetch(api_url, {
+         method: 'POST',
+         body: JSON.stringify({
+            url:vidurl
+         }),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+         },
+      })
+         .then((res) => res.json())
+         .then((post) => {
+            // setPosts((posts) => [post, ...posts]);
+            setPosts(post)
+            setLoading('')
+            
+         })
+         .catch((err) => {
+            setErr(err.message)
+            console.log(err.message);
+            setLoading('')
+         });
+
+}
+// console.log(post)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+   <div className="Formbox">
+   <h1>You Vid</h1>
+   <form onSubmit={subm}>
+
+<center>
+<input type="text" required value={vidurl}  onChange={(e)=>{setVidurl(e.target.value)}}></input>
+    <br></br>
+    <button type="submit">Submit</button>
+</center>
+   </form>
+   <br></br>
+   {/* {post.map((po)=>{
+      // console.log(po.qualityLabel)
+      <><div>
+         {po.url}
+      </div></>
+   })} */}
+   <hr></hr>
+   {<h1>{err}</h1>}
+   {post.map(po=>
+      
+      <><div><h2>{po.qualityLabel}</h2><a href={po.url}><button>Download</button></a><p>{po.mimeType}</p></div><br></br><hr></hr>
+      
+      </>
+   ) }
+
+   {<h2>{status}</h2>}
+   {<h2>{loading}</h2>}
+</div>
+    </>
   );
 }
 
